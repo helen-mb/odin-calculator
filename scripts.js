7566b32cb6e951f3183bd974a2e4154af7dff282
Helen Burger <burger.helen.m@gmail.com>
2022-10-19 15:35:10 -0700

    Make equals button call the operate function to execute the operator

--- scripts.js
+++ scripts.js
@@ -9,24 +9,33 @@ const equalsBtn = document.getElementById('equalsBtn');
 const clearBtn = document.getElementById('clearBtn');
 
 //The basic math operations between two numbers:
-const add = function (a, b) {
+
+function add(a, b) {
     return a + b;
 }
 
-const subtract = function (a, b) {
+function subtract(a, b) {
     return (a - b);
 }
 
-const multiply = function (a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-const divide = function (a, b) {
+function divide(a, b) {
     return a / b;
 }
 
-const operate = function (operator, a, b) {
-    return operator(a, b);
+//NOTE: opName is not recognized as a function type, only a string, hence why using it as a keyword for a callback function did not work?
+let runningTotal = null; 
+function operate(opName, a, b) {
+    if (opName === 'add') {runningTotal = add(+a, +b)}
+    else if (opName === 'subtract') {runningTotal = subtract(a, b)}
+    else if (opName === 'multiply') {runningTotal = multiply(a, b)}
+    else if (opName === 'divide') {runningTotal = divide(a, b)}
+    display.value = null;
+    console.log(runningTotal);
+    return runningTotal;
 }
 
 //Printing the numbers to the display on click
@@ -34,13 +43,14 @@ numKeys.forEach((numBtn) => numBtn.addEventListener('click', e => display.value 
 
 //TODO: refresh the display value only after operator is clicked
 //Prepping the operate function with the click of an operator button
-let runningTotal = null; // presently unused
-let operandA = null;
+let storedValue = null;
 let opName = null;
 const hold = (e) => {
-    operandA = display.value;
+    storedValue = display.value;
     opName = e.target.value;
-    display.value = null;
-    console.log(operandA, opName, display.value);
+    display.value = null; // presently clears the display screen entirely
 }
 opKeys.forEach((opBtn) => opBtn.addEventListener('click', hold));
+
+//Running the operate function with the click of the equals button
+equalsBtn.addEventListener('click', () => operate(opName, storedValue, display.value));
