b9395ea013d1d80f1a1995a8f97033f9de5bf6d3
Helen Burger <burger.helen.m@gmail.com>
2022-10-19 12:25:47 -0700

    Hold operand and operator on click of an opBtn

--- scripts.js
+++ scripts.js
@@ -1,14 +1,12 @@
 const display = document.getElementById('display');
 const numKeys = document.querySelectorAll('button.num');
-const operators = document.querySelectorAll('button.operator');
+const opKeys = document.querySelectorAll('button.operator');
 const addBtn = document.getElementById('addBtn');
 const subtractBtn = document.getElementById('subtractBtn');
 const multiplyBtn = document.getElementById('multiplyBtn');
 const divideBtn = document.getElementById('divideBtn');
 const equalsBtn = document.getElementById('equalsBtn');
 const clearBtn = document.getElementById('clearBtn');
-
-let runningTotal = null;
 
 //The basic math operations between two numbers:
 const add = function (a, b) {
@@ -32,8 +30,17 @@ const operate = function (operator, a, b) {
 }
 
 //Printing the numbers to the display on click
-numKeys.forEach((numKey) => numKey.addEventListener('click', e => display.value += e.target.value));
+numKeys.forEach((numBtn) => numBtn.addEventListener('click', e => display.value += e.target.value));
 
-//FIXME:
-//Populating operate function with the click of an operator button
-operators.forEach((operator) => operator.addEventListener('click', e => operate(e.target.value, [display.value, null])));
+//TODO: refresh the display value only after operator is clicked
+//Prepping the operate function with the click of an operator button
+let runningTotal = null; // presently unused
+let operandA = null;
+let opName = null;
+const hold = (e) => {
+    operandA = display.value;
+    opName = e.target.value;
+    display.value = null;
+    console.log(operandA, opName, display.value);
+}
+opKeys.forEach((opBtn) => opBtn.addEventListener('click', hold));
